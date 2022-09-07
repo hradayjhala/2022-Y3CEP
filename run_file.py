0ffb592bd40d45e66757f28a1b535cbd6e4c9686
hradayjhala <75161221+hradayjhala@users.noreply.github.com>
2022-09-07 12:16:54 +0800

    fixed the wrong function error

--- run_file.py
+++ run_file.py
@@ -2,7 +2,7 @@ from midi_reader import MidiReader
 from song_importer import SongImporter
 from music_generator import MusicGenerator
 
-coordinator = MidiCoordinator(24, 102)
+reader = MidiReader(24, 102)
 importer = SongImporter('midi_songs', coordinator)
 generator = MusicGenerator(coordinator)
 
